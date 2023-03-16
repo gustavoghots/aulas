1ea198a7c45b00de573eb44648efdf00b4998bb9
gustavoghots <gustavo.fiss.v@gmail.com>
2023-03-16 11:22:14 -0300

    Create index.php

---
+++ lpw/aula3/index.php
@@ -0,0 +1,25 @@
+<html>
+	<body>
+		<form method="POST" action="aula3.php">
+            <div>
+                <h1>Hotwheels</h1>
+                <label>Modelo:</label><br>
+                <input type="text" name="modelo" required><br><br>
+                <label>Cor:</label><br>
+                <input type="text" name="cor" required><br><br>
+                <label>Ano:</label><br>
+                <input type="text" name="ano" required><br><br>
+            </div>
+            <div>
+                <h1>Pessoa</h1>
+                <label>Nome:</label><br>
+                <input type="text" name="nome" required><br><br>
+                <label>Idade:</label><br>
+                <input type="text" name="idade" required><br><br>
+                <label>Endereço:</label><br>
+                <input type="text" name="endereco" required><br><br>
+                <input type="submit" value="Enviar"><br>
+            </div>
+		</form>
+	</body>
+</html>
