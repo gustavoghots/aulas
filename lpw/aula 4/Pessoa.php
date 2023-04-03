ed623069502b7a0a513fd71e589671fc364233d9
gustavoghots <gustavo.fiss.v@gmail.com>
2023-04-03 14:48:55 -0300

    Create Pessoa.php

---
+++ lpw/aula 4/Pessoa.php
@@ -0,0 +1,26 @@
+<?php
+class Pessoa {
+  private $nome;
+  private $idade;
+
+  function __construct($nome, $idade) {
+    $this->nome = $nome;
+    $this->idade = $idade;
+  }
+  public function getNome() {
+    return $this->nome;
+  }
+
+  public function setNome($nome) {
+    $this->nome = $nome;
+  }
+
+  public function getIdade() {
+    return $this->idade;
+  }
+
+  public function setIdade($idade) {
+    $this->idade = $idade;
+  }
+}
+?>
