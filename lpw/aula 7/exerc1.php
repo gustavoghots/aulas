99610a720c15f8858ce6b75deb3b59cda85df5d5
gustavoghots <gustavo.fiss.v@gmail.com>
2023-04-27 14:54:47 -0300

    Create exerc1.php

---
+++ lpw/aula 7/exerc1.php
@@ -0,0 +1,31 @@
+<html>
+	<body>
+		<form method="POST" action="">
+      <h1>Pessoa</h1>
+      <label>Nome:</label><br>
+      <input type="text" name="nome" required><br><br>
+      <label>Idade:</label><br>
+      <input type="text" name="idade" required><br><br>
+      <label>Cpf:</label><br>
+      <input type="text" name="cpf" required><br><br>
+      <input type="submit" value="Enviar"><br><br><br>
+		</form>
+	</body>
+</html>
+<?php
+  if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    include_once 'fisica.php';
+    include_once 'juridica.php';
+    include_once 'pessoa.php';
+    $f[0] = new Fisica();
+    $f[0]->SetNome($_POST["nome"]);
+    $f[0]->SetIdade($_POST['idade']);
+    $f[0]->SetCpf($_POST['cpf']);
+    echo "<h2>Dados da Pessoa:</h2>";
+            echo "<ul>";
+            echo "<li>Nome: " . $f[0]->getNome() . "</li>";
+            echo "<li>Idade: " . $f[0]->getIdade() . "</li>";
+            echo "<li>Cpf: " . $f[0]->getCPF() . "</li>";
+            echo "</ul>";
+  }
+?>
