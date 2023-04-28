491c05f214daa40fdaf226537bcfd1b69d2a3931
gustavoghots <gustavo.fiss.v@gmail.com>
2023-04-27 21:51:42 -0300

    Update pessoa.php

--- lpw/aula 7/pessoa.php
+++ lpw/aula 7/pessoa.php
@@ -3,6 +3,11 @@ abstract class Pessoa {
   private $nome;
   private $idade;
 
+  function __construct($nome, $idade) {
+    $this->nome = $nome;
+    $this->idade = $idade;
+  }
+
   public function getNome() {
     return $this->nome;
   }
