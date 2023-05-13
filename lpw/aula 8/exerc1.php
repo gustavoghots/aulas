0cb02165c25b80954a6ed500878b1f8827f597cb
gustavoghots <gustavo.fiss.v@gmail.com>
2023-05-13 13:59:54 -0300

    On branch main

--- lpw/aula 8/exerc1.php
+++ lpw/aula 8/exerc1.php
@@ -14,9 +14,26 @@
     $turma[] = new Aluno('Manuela','17','???','1');
     $turma[] = new Aluno('Isis','17','???','6');
 
-    echo $if->GetNome()."<br".$if->GetIdade()."<br".$if->GetCnpj();
-    echo $professor->GetNome()."<br>".$professor->GetIdade()."<br>".$professor->GetCpf()."<br>".$professor->GetSalario();
+    echo "<h2>Dados do IFsul:</h2>";
+        echo "<ul>";
+        echo "<li>Nome: " . $if->GetNome() . "</li>";
+        echo "<li>Idade: " . $if->GetIdade() . "</li>";
+        echo "<li>Cpf: " . $if->GetCnpj() . "</li>";
+        echo "</ul><br><br>";
+    echo "<h2>Dados do professor:</h2>";
+        echo "<ul>";
+        echo "<li>Nome: " . $professor->GetNome() . "</li>";
+        echo "<li>Idade: " . $professor->GetIdade() . "</li>";
+        echo "<li>Cpf: " . $professor->GetCpf() . "</li>";
+        echo "<li>Nota: " . $professor->GetSalario() . "</li>";
+        echo "</ul><br><br>";
     foreach ($turma as $turma){
-        $turma->GetNome()."<br>".$turma->GetIdade()."<br>".$turma->GetCpf()."<br>".$turma->GetNota();
+        echo "<h2>Dados dos alunos:</h2>";
+        echo "<ul>";
+        echo "<li>Nome: " . $turma->GetNome() . "</li>";
+        echo "<li>Idade: " . $turma->GetIdade() . "</li>";
+        echo "<li>Cpf: " . $turma->GetCpf() . "</li>";
+        echo "<li>Nota: " . $turma->GetNota() . "</li>";
+        echo "</ul>";
     }
 ?>
