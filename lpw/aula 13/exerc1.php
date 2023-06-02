54c89e9bcca965f6a8ca5ffb5b3dfeb3aa44d9d2
gustavoghots <gustavo.fiss.v@gmail.com>
2023-06-02 08:20:11 -0300

    Update exerc1.php

--- lpw/aula 13/exerc1.php
+++ lpw/aula 13/exerc1.php
@@ -1,7 +1,20 @@
 <?php
-    $a[]=20;
-    $a[]=20;
-    $a['oi']=20;
-    $a[]=20;
-    echo $a[1];
+    include_once 'pessoa.php';
+    for($i=0;$i<1000;$i++){
+        $lista[]= new Pessoa();
+        $lista[$i]->setIdade(rand(18,99));
+        $lista[$i]->setNome(randString(3));
+    }
+    function randString($tamPalavra){
+        $chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        $tamVar = strlen($chars);
+        $retorno="";
+        for( $x = 0; $x < $tamPalavra; $x++ )
+        $retorno = $retorno.(String) $chars[ rand( 0, $tamVar - 1 ) ];
+        return $retorno;
+    }
+
+    foreach ($lista as $lista){
+        echo  $lista->getNome().' - '.$lista->getIdade()."<br>";
+    }
 ?>
