c5d37f5c64274b91d4f0c4d24fffc3c4b3339572
gustavoghots <gustavo.fiss.v@gmail.com>
2023-07-03 16:17:58 -0300

    Create formulario2.php

---
+++ lpw/aula18/formulario2.php
@@ -0,0 +1,10 @@
+<html>
+    <form method="POST" action="verificao.php">
+        <h1>Consulta</h1>
+        <label>Login:</label><br>
+        <input type="text" name="login"><br><br>
+        <label>Senha:</label><br>
+        <input type="text" name="senha"><br><br>
+        <input type="submit" value="Enviar">
+    </form>
+</html>
