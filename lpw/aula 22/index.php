77ccb38f65679ef5b585cb63b8f05b8b3ae77c0e
gustavoghots <gustavo.fiss.v@gmail.com>
2023-08-17 11:13:30 -0300

    diversão

--- lpw/aula 22/index.php
+++ lpw/aula 22/index.php
@@ -5,6 +5,8 @@
             <input type="text" name="cidade" required><br><br>
             <label>Nome estado:</label><br>
             <input type="text" name="estado" required><br><br>
+            <label>ID Cidade:</label><br>
+            <input type="number" name="id"><br><br>
             <input type="submit" name="insert" value="insert">
             <input type="submit" name="delete" value="delete">
         </form>
@@ -18,7 +20,7 @@
             $estado = mysqli_fetch_assoc($estado);
             mysqli_query($con,"insert into cidade(nome,estado) values('".$_POST['cidade']."',".$estado['id'].")");
         }elseif(isset($_POST['delete'])){
-            mysqli_query($con,"delete from cidade where nome = '".$_POST['cidade']."' and id>0;");
+            mysqli_query($con,"delete from cidade where id=".$_POST['id']);
         }
         $cidades = mysqli_query($con,"
         select c.nome as nome_cidade, e.nome as nome_estado
