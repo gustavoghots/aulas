7c925ceda0e971c72afd01103429eb8a27530b38
gustavoghots <gustavo.fiss.v@gmail.com>
2023-09-12 14:07:25 -0300

    Create conexão.php

---
+++ lpw/aula 24/conexão.php
@@ -0,0 +1,7 @@
+<?php
+    $con = mysqli_connect("localhost","root","","estados_cidades");
+    if (mysqli_connect_errno()){
+        echo "<script>console.log('falha ao conectar ao banco')</script>";
+        exit();
+    }
+?>
